8f80a7883d45311a6ef4d55484a650ee53155e74
akwing <wqant@foxmail.com>
2016-06-07 17:04:12 +0800

    Update tmap.h
    为污染属性类和tmap类添加了合并功能

--- ASTReader/tmap.h
+++ ASTReader/tmap.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <vector>
+
 #include "clang/Frontend/ASTUnit.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Basic/FileSystemOptions.h"
@@ -9,12 +11,14 @@ using namespace std;
 using namespace clang;
 using namespace llvm;
 
+//污染情况
 enum e_tattr{
-		TAINTED,
-		UNTAINTED,
-		RELATED
+	TAINTED,
+	UNTAINTED,
+	RELATED
 };
 
+//变量的污染属性
 class Tainted_Attr
 {
 public:
@@ -25,21 +29,21 @@ public:
 		relation = 0;
 	}
 	//拷贝构造函数
-	Tainted_Attr(const Tainted_Attr& b)
+	Tainted_Attr(Tainted_Attr& b)
 	{
 		attr = b.attr;
-		relation = b.relation();
+		relation = b.relation;
 	}
 	//信息输出函数 调试用
 	void output()
 	{
 		if (attr == TAINTED)
-			cout << "TAINTED ";
+			std::cout << "TAINTED ";
 		else if (attr == UNTAINTED)
-			cout << "UN ";
+			std::cout << "UN ";
 		else
-			cout << "RE ";
-		cout << relation << endl;
+			std::cout << "RE ";
+		std::cout << relation << endl;
 	}
 	//信息设置函数
 	void attr_set(e_tattr a, unsigned long long r)
@@ -47,9 +51,32 @@ public:
 		attr = a;
 		relation |= r;
 	}
+	//将两个污染属性取并
+	void AndAttr(Tainted_Attr &b)
+	{
+		if (attr == TAINTED)
+			return;
+		if (b.attr == TAINTED)
+		{
+			attr = TAINTED;
+			return;
+		}
+		if (attr == UNTAINTED)
+		{
+			attr = b.attr;
+			relation = b.relation;
+			return;
+		}
+		if (b.attr == UNTAINTED)
+			return;
+		attr = b.attr;
+		relation |= b.relation;
+	}
 private:
-	e_tattr attr;	//污染属性
-	unsigned long long relation;	//污染与哪些变量相关
+	//污染属性
+	e_tattr attr;
+	//污染与哪些变量相关
+	unsigned long long relation;
 };
 
 //封装了C++ map模板的污染表类 
@@ -59,7 +86,7 @@ public:
 	//构造函数
 	Tmap(){}
 	//拷贝构造函数
-	Tmap(const Tmap& b)
+	Tmap(Tmap& b)
 	{
 		Tainted_Attr *t = NULL, *newattr;
 		VarDecl *pdec = NULL;
@@ -95,13 +122,14 @@ public:
 	{
 		Tainted_Attr *t;
 		map<VarDecl *, Tainted_Attr *>::iterator iter = tmap.begin(), iter_end = tmap.end();
-		
+
 		while (iter != iter_end)
 		{
 			t = (*iter).second;
 			t->output();
 		}
 	}
+	//若p不在表中，插入一个变量定义节点，并创建一个污染属性变量
 	void insert(VarDecl *p)
 	{
 		Tainted_Attr *t = new Tainted_Attr();
@@ -113,23 +141,35 @@ public:
 		else
 			delete t;
 	}
+	//从map中删除p
 	void del(VarDecl *p)
 	{
 		Tainted_Attr *t = tmap[p];
 		delete t;
 		tmap.erase(p);
 	}
+	//取得变量定义节点p对应的污染属性
 	Tainted_Attr *getmap(VarDecl *p)
 	{
-		Tainted_Attr *t;
 		int count;
 		count = tmap.count(p);
 		if(count == 0)
 			return NULL;
 		else
 			return tmap[p];
-	} 
-
+	}
+	//将两个map中的污染属性合并
+	void AndMap(Tmap &b)
+	{
+		VarDecl *p;
+		map<VarDecl *, Tainted_Attr *>::iterator iter = tmap.begin(), iter_end = tmap.end();
+		while (iter != iter_end)
+		{
+			p = (*iter).first;
+			if (b.getmap(p) != NULL)
+				(*iter).second->AndAttr(*b.getmap(p));
+		}
+	}
 private:
 	map<VarDecl *, Tainted_Attr *> tmap;
 };
