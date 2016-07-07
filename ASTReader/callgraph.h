46cb8eb0b5f4cf03bb23360344b91b50720a5f9e
tobuer <tobuer2@hotmail.com>
2016-07-07 19:33:53 +0800

    Create callgraph.h
    将callgraph相关从AST.h中拆分出来

---
+++ ASTReader/callgraph.h
@@ -0,0 +1,59 @@
+#include "AST.h"
+
+typedef enum
+{
+	common, inclass
+}methodType;
+
+//函数调用关系图
+class callgraph{
+public:
+	callgraph(FunctionDecl* f1);
+	callgraph(FunctionDecl* f1, FunctionDecl* f2);
+	FunctionDecl* getCaller(int i);		//调用cur
+	FunctionDecl* getCallee(int i);		//被cur调用
+	FunctionDecl* getCur();
+	int getCallerNum();
+	int getCalleeNum();
+	void addCaller(FunctionDecl* otherFD);
+	void addCallee(FunctionDecl* otherFD);
+	void delCallee(FunctionDecl* otherFD);
+	void changeMethodType();
+	void setRoot(VarDecl* r);
+	void setClass(CXXRecordDecl* rd);
+	CXXRecordDecl* getClass();
+	VarDecl* getRoot();
+	methodType getMethodType();
+	int ifCheck;
+
+public:
+	std::unique_ptr<CFG> get_cfg();
+	void print_cfg();
+	CTmap& getCTmap();
+	void addParam(VarDecl* vd);
+	void addVar(VarDecl* vd);
+	int getParamNum();
+	int getVarNum();
+
+private:
+	//方法的类型
+	methodType type;
+	//类方法所属的实例
+	VarDecl* root;
+	CXXRecordDecl* classDecl;
+	FunctionDecl* caller[10];
+	FunctionDecl* cur;
+	FunctionDecl* callee[10];
+	//std::unique_ptr<CFG> cfg;
+	int callerNum, calleeNum;
+	CTmap* map;
+	int paramNum;
+	int varNum;
+	Tainted_Attr* returnVar;
+};
+
+callgraph* findById(std::vector<callgraph*> Callgraph, std::string id);
+void ifcheck(std::vector<callgraph*> cg, callgraph* t);
+void resetIfCheck(std::vector<callgraph*>Callgraph);
+void getRing(std::vector<callgraph*>& Callgraph, int n, std::vector<FunctionDecl*>& ringVector);
+void printCallGraph(std::vector<callgraph*> Callgraph);
