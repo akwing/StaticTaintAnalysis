7642817627902a50050fdc9d33e2246cd0a00f12
tobuer <tobuer2@hotmail.com>
2016-07-19 17:03:55 +0800

    Update callgraph.h

--- ASTReader/callgraph.h
+++ ASTReader/callgraph.h
@@ -1,41 +1,79 @@
-#ifndef CLASSTMAP_H
-#define CLASSTMAP_H
+#ifndef CALLGRAPH_H
+#define CALLGRAPH_H
+
+class CFGInOut;
+
 #include "AST.h"
+#include "CFGtattr.h"
+#include "TaintedChangeInfo.h"
 
-using namespace clang;
-using namespace std;
-
-class classTmap
+typedef enum
 {
+	common, inclass
+}methodType;
+
+//函数调用关系图
+class callgraph{
 public:
-	classTmap();
-	void setCXXRecordDecl(CXXRecordDecl* cxxrd);
-	void addMethod(CXXMethodDecl* md);
-	void addVar(FieldDecl* fd);
-	const std::vector<CXXMethodDecl*>& get_cxxmds();
-	const std::vector<FieldDecl*>& get_fds();
-	CXXRecordDecl* get_cxxrd()const;
-	int getMethodNum();
+	callgraph(FunctionDecl* f1);
+	callgraph(FunctionDecl* f1, FunctionDecl* f2);
+	std::vector<FunctionDecl*>& getCaller();		//调用cur
+	std::vector<FunctionDecl*>& getCallee();		//被cur调用
+	FunctionDecl* getCur();
+	int getCallerNum();
+	int getCalleeNum();
+	void addCaller(FunctionDecl* otherFD);
+	void addCallee(FunctionDecl* otherFD);
+	void delCallee(FunctionDecl* otherFD);
+	void changeMethodType();
+	void setRoot(VarDecl* r);
+	void setClass(CXXRecordDecl* rd);
+	CXXRecordDecl* getClass();
+	VarDecl* getRoot();
+	methodType getMethodType();
+	int ifCheck;
+	bool is_caller(FunctionDecl* fd);
+	bool is_callee(FunctionDecl* fd);
+
+public:
+	std::unique_ptr<CFG>& get_cfg();
+	void print_cfg();
+	CTmap& getCTmap();
+	void addParam(VarDecl* vd);
+	void addVar(VarDecl* vd);
+	int getParamNum();
 	int getVarNum();
-	CTmap* getMap();
+	Tainted_Attr* getReturn();
+	void setReturn(Tainted_Attr* temp);
+	void set_if_check_cfg();
 
-//public://for tmap
-//+	classTmap
-
+public:
+	std::map<clang::CFGBlock *, CFGInOut> block_io_map;
+	vector<TCI*> TCI_list;
 
 private:
-	CXXRecordDecl* rd;
-	std::vector<CXXMethodDecl*> cxxmds;
-	std::vector<FieldDecl*> fds;
-	CTmap map;
-	int methodNum;
+	//方法的类型
+	methodType type;
+	//类方法所属的实例
+	VarDecl* root;
+	CXXRecordDecl* classDecl;
+	std::vector<FunctionDecl*> caller;
+	FunctionDecl* cur;
+	std::vector<FunctionDecl*> callee;
+	std::unique_ptr<CFG> cfg;
+	int callerNum, calleeNum;
+	CTmap* map;
+	int paramNum;
 	int varNum;
-	//int publicVarNum;
-	//int privateVarNum;
-	//int protectedVarNum;
+	Tainted_Attr* returnVar;
+	bool if_check_cfg;
 };
 
-void printClassTmap(std::vector<classTmap*> CT);
-bool if_find_class(vector<classTmap*>ClassTmap, CXXRecordDecl* rd);
+callgraph* findById(std::vector<callgraph*> Callgraph, std::string id);
+void ifcheck(std::vector<callgraph*> cg, callgraph* t);
+void resetIfCheck(std::vector<callgraph*>Callgraph);
+void getRing(std::vector<callgraph*>& Callgraph, int n, std::vector<FunctionDecl*>& ringVector);
+void printCallGraph(std::vector<callgraph*> Callgraph);
 
+bool if_find_function(std::vector<callgraph*> Callgraph,FunctionDecl* fd);
 #endif
