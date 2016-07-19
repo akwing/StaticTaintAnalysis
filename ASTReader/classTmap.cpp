76eaea19b8b65f3e004dffb3ee71b53b5936012b
tobuer <tobuer2@hotmail.com>
2016-07-19 17:00:22 +0800

    Update classTmap.cpp

--- ASTReader/classTmap.cpp
+++ ASTReader/classTmap.cpp
@@ -20,11 +20,11 @@ void classTmap::addVar(FieldDecl* fd)
 	fds.push_back(fd);
 	varNum++;
 }
-const std::vector<CXXMethodDecl*>& classTmap::get_cxxmds()
+const vector<CXXMethodDecl*>& classTmap::get_cxxmds()
 {
 	return cxxmds;
 }
-const std::vector<FieldDecl*>& classTmap::get_fds()
+const vector<FieldDecl*>& classTmap::get_fds()
 {
 	return fds;
 }
@@ -41,39 +41,39 @@ int classTmap::getVarNum()
 	return varNum;
 }
 
-void printClassTmap(std::vector<classTmap*> CT)
+void printClassTmap(vector<classTmap*> CT)
 {
-	std::vector<classTmap*>::iterator it_ct = CT.begin(), it_ct_end = CT.end();
-	std::vector<CXXMethodDecl*>::iterator it_cxxmd;
-	std::vector<CXXMethodDecl*> cxxmds;
+	vector<classTmap*>::iterator it_ct = CT.begin(), it_ct_end = CT.end();
+	vector<CXXMethodDecl*>::iterator it_cxxmd;
+	vector<CXXMethodDecl*> cxxmds;
 	int methodNum;
-	std::vector<FieldDecl*>::iterator it_fd;
-	std::vector<FieldDecl*>fds;
+	vector<FieldDecl*>::iterator it_fd;
+	vector<FieldDecl*>fds;
 	int varNum;
 	for (; it_ct != it_ct_end; it_ct++)
 	{
 		cxxmds = (*it_ct)->get_cxxmds();
 		fds = (*it_ct)->get_fds();
-		std::cout << (*it_ct)->get_cxxrd()->getQualifiedNameAsString() << "\n";
+		cout << (*it_ct)->get_cxxrd()->getQualifiedNameAsString() << "\n";
 		it_cxxmd = cxxmds.begin();
 		methodNum = (*it_ct)->getMethodNum();
 		it_fd = fds.begin();
 		varNum = (*it_ct)->getVarNum();
-		std::cout << "\tVar:\n";
+		cout << "\tVar:\n";
 		if (fds.size() > 0)
 		{
 			for (int i = 0; i < varNum; i++)
 			{
-				std::cout << "\t\t" << (*it_fd)->getQualifiedNameAsString() << "\n";
+				cout << "\t\t" << (*it_fd)->getQualifiedNameAsString() << "\n";
 				it_fd++;
 			}
 		}
 		if (cxxmds.size() > 0)
 		{
-			std::cout << "\tMethod:\n";
+			cout << "\tMethod:\n";
 			for (int i = 0; i < methodNum; i++)
 			{
-				std::cout << "\t\t" << (*it_cxxmd)->getQualifiedNameAsString() << "\n";
+				cout << "\t\t" << (*it_cxxmd)->getQualifiedNameAsString() << "\n";
 				it_cxxmd++;
 			}
 		}
@@ -87,7 +87,7 @@ bool if_find_class(vector<classTmap*>ClassTmap, CXXRecordDecl* rd)
 	vector<classTmap*>::iterator it = ClassTmap.begin(), it_end = ClassTmap.end();
 	for (; it != it_end; it++)
 	{
-		if ((*it)->get_cxxrd() == rd)
+		if ((*it)->get_cxxrd() == rd) 
 			return true;
 	}
 	return false;
@@ -96,4 +96,48 @@ bool if_find_class(vector<classTmap*>ClassTmap, CXXRecordDecl* rd)
 CTmap* classTmap::getMap()
 {
 	return &map;
+}
+
+void classTmap::classCopy(classTmap* temp)
+{
+	rd = temp->rd;
+
+	cxxmds.clear();
+	vector<CXXMethodDecl*>::iterator it_cxxmd=temp->cxxmds.begin(),
+		it_cxxmd_end=temp->cxxmds.end();
+	for (; it_cxxmd != it_cxxmd_end; it_cxxmd++)
+	{
+		cxxmds.push_back((*it_cxxmd));
+	}
+	
+	fds.clear();
+	vector<FieldDecl*>::iterator it_fd = temp->fds.begin(),
+		it_fd_end = temp->fds.end();
+	for (; it_fd != it_fd_end; it_fd++)
+	{
+		fds.push_back((*it_fd));
+	}
+
+	map.CopyMap(temp->map);
+
+	methodNum = temp->methodNum;
+	varNum = temp->varNum;
+
+}
+
+void classTmap::classUnion(classTmap* m,classTmap* a, classTmap* b)
+{
+	if (a->rd != b->rd)
+		cout << "不允许不同的classTmap的合并" << endl;
+
+	//classTmap map_re;
+	m->classCopy(a);
+	m->map.unionMap(b->map);
+}
+
+void classTmap::classClear()
+{
+	cxxmds.clear();
+	fds.clear();
+	map.clear();
 }
