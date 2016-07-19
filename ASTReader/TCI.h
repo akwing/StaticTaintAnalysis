eecf82374390e7f6783379bc6d06985e56aa9c48
tobuer <tobuer2@hotmail.com>
2016-07-19 14:19:37 +0800

    Create TCI.h

---
+++ ASTReader/TCI.h
@@ -0,0 +1,20 @@
+#ifndef TCI_H
+#define TCI_H
+
+#include "AST.h"
+#include "tmap.h"
+
+using namespace clang;
+using namespace std;
+using namespace llvm;
+
+class TCI
+{
+public:
+	VarDecl* vd;
+	Stmt* stmt;
+	Tainted_Attr* be;
+	Tainted_Attr* af;
+};
+
+#endif
