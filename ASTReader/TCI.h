ac1e9f819fc574aba3987afa87feabbba1f00cb2
tobuer <tobuer2@hotmail.com>
2016-07-20 18:24:21 +0800

    Update TCI.h

--- ASTReader/TCI.h
+++ ASTReader/TCI.h
@@ -8,6 +8,14 @@ using namespace clang;
 using namespace std;
 using namespace llvm;
 
+//敏感信息类型
+#define TTYPE_ARRAY
+#define TTYPE_DIV_MOD
+#define TTYPE_LOOP_BOUND
+#define TTYPE_MEM
+#define TTYPE_POINTER_NULL_SET
+#define TTYPE_RECURSIVE_CALL
+
 typedef struct
 {
 	int type;
